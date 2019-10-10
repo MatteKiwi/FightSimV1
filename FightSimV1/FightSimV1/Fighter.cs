ad3119923b47b71adb71b15c241d2e90a17c5a1d
MatteKiwi <55237242+MatteKiwi@users.noreply.github.com>
2019-10-10 13:19:57 +0200

    comments/övning v1 klar

--- FightSimV1/FightSimV1/Fighter.cs
+++ FightSimV1/FightSimV1/Fighter.cs
@@ -8,21 +8,22 @@ namespace FightSimV1
 {
     class Fighter
     {
+        //variabler 
         private int hp = 100;
         public string name;
         public static Random gen = new Random();
-
+        //generara en random nummer för attack
         public int Attack()
         {
            int i = gen.Next(5, 30);
            return i;
         }
-        
+        //tar bort hp
         public void Hurt(int amount)
         {
             hp -= amount;
         }
-
+        //kollar ifall fighter lever
         public bool IsAlive()
         {
             if (hp < 0)
@@ -36,7 +37,7 @@ namespace FightSimV1
                 return false;
             }
         }
-
+        //retunerar hp
         public int GetHp()
         {
             int x = hp;
--- FightSimV1/FightSimV1/Program.cs
+++ FightSimV1/FightSimV1/Program.cs
@@ -10,20 +10,23 @@ namespace FightSimV1
     {
         static void Main(string[] args)
         {
+            //skapar två instanser av fighter
             Fighter A = new Fighter();
             Fighter B = new Fighter();
-
+            //ger dom namn för jag är lat och orkar inte ge använderen frihet att namnge dom
             A.name = "Juan";
             B.name = "Carlos";
-          
-
+            //välkommnar användaren
+            Console.WriteLine("Welcome to fight sim, just press enter and enojoy");
+            //whilpe loop med hela spelet
             while (true)
             {
+                //b tar skade av a attack och tvärtom för a
                 B.Hurt(A.Attack());
                 A.Hurt(B.Attack());
-
+                //skriver ut namn samt hp för båda fighters
                 Console.WriteLine(A.name + " Hp: " + A.GetHp() + " " + B.name + " Hp: " + B.GetHp());
-
+                //if sats som kollar ifall dom lever och visar då vem som vann
                 if(A.IsAlive() == true)
                 {
                     Console.WriteLine(B.name + " Won !");
