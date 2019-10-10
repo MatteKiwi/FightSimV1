df94031da38eb9a58e1c979e2cc298f996af8f95
MatteKiwi <55237242+MatteKiwi@users.noreply.github.com>
2019-10-10 13:00:38 +0200

    fighter

---
+++ FightSimV1/FightSimV1/Fighter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV1
+{
+    class Fighter
+    {
+        private int hp = 100;
+        public string name;
+        public Random gen = new Random();
+
+        public int Attack()
+        {
+           int i = gen.Next(5, 30);
+           return i;
+        }
+        
+        public void Hurt(int amount)
+        {
+            hp -= amount;
+        }
+
+        public bool IsAlive()
+        {
+            if (hp < 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHp()
+        {
+            int x = hp;
+            return x;
+        }
+
+        
+    }
+}
